33fddf5637f06774ee677bbe84c5c69366287be0
Irina Malovastaja <irina.malovastaja@yandex.ru>
2022-04-11 01:00:46 +0300

    create functional for 8 lesson

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,7 +2,7 @@
 @section('title')Пользователи@endsection
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Категории новостей</h1>
+        <h1 class="h2">Пользователи</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
                 <a id="saveForm" type="button" class="btn btn-sm btn-outline-secondary">Сохранить</a>
